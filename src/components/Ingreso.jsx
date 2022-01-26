dc4ce86ceed0d9db9414b6b8277c35bd18676d01
Nicolás García <nicolasg.ngl@gmail.com>
2022-01-25 21:13:31 -0500

    correccion errore

--- src/components/Ingreso.jsx
+++ src/components/Ingreso.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios'
 import { imgUpload } from '../helpers/imgUpload';
 import { url } from '../helpers/url';
